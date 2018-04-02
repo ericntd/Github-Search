61bd75f8615bcd8d2fbb170ef4327c16d04abab5
Eric N <ericntd@gmail.com>
2018-04-02 18:17:30 +0800

    Perfected SearchPresenterTest

--- app/src/test/java/tech/ericntd/githubsearch/search/SearchPresenterTest.java
+++ app/src/test/java/tech/ericntd/githubsearch/search/SearchPresenterTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Response;
@@ -52,15 +53,6 @@ public class SearchPresenterTest {
         Mockito.verify(repository, Mockito.times(1)).searchRepos(searchQuery, presenter);
     }
 
-    /**
-     * By default, <code>Response response = Mockito.mock(Response.class)</code> will not work
-     * since Retrofit's Response is final class
-     * you will see an error "Mockito cannot mock/spy because : - final class"
-     * <p>
-     * That problem is overcome for Mockito 2+ following the instruction <a href="https://github
-     * .com/mockito/mockito/wiki/What's-new-in-Mockito-2#mock-the-unmockable-opt-in-mocking-of
-     * -final-classesmethods">here</a>
-     */
     @SuppressWarnings("unchecked")
     @Test
     public void handleGitHubResponse_Success() {
@@ -68,45 +60,49 @@ public class SearchPresenterTest {
         SearchResponse searchResponse = Mockito.mock(SearchResponse.class);
         Mockito.doReturn(true).when(response).isSuccessful();
         Mockito.doReturn(searchResponse).when(response).body();
-        List<SearchResult> searchResults = new ArrayList<>();
-        searchResults.add(new SearchResult());
-        searchResults.add(new SearchResult());
-        searchResults.add(new SearchResult());
+        List<SearchResult> searchResults = Collections.singletonList(new SearchResult());
         Mockito.doReturn(searchResults).when(searchResponse).getSearchResults();
 
+        // Trigger
         presenter.handleGitHubResponse(response);
 
+        // Validation
         Mockito.verify(viewContract, Mockito.times(1)).displaySearchResults(searchResults);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void handleGitHubResponse_Failure() {
         Response response = Mockito.mock(Response.class);
-
-        // case No. 1
         Mockito.doReturn(false).when(response).isSuccessful();
 
+        // Trigger
         presenter.handleGitHubResponse(response);
 
-        Mockito.verify(viewContract, Mockito.times(1)).displayError(Mockito.anyString());
-        /*
-        if the following is not called, viewContract.displayError() will be
-        mistakenly considered as invoked twice
-         */
-        Mockito.reset(viewContract);
+        // Validation
+        Mockito.verify(viewContract, Mockito.times(1)).displayError("E101 - System error");
+    }
 
-        // case No. 2
+    @SuppressWarnings("unchecked")
+    @Test
+    public void handleGitHubResponse_EmptyResponse() {
+        Response response = Mockito.mock(Response.class);
+        Mockito.doReturn(true).when(response).isSuccessful();
         Mockito.doReturn(null).when(response).body();
 
+        // Trigger
         presenter.handleGitHubResponse(response);
 
-        Mockito.verify(viewContract, Mockito.times(1)).displayError(Mockito.anyString());
+        // Validation
+        Mockito.verify(viewContract, Mockito.times(1)).displayError("E102 - System error");
     }
 
     @Test
     public void handleGitHubError() {
+        // Trigger
         presenter.handleGitHubError();
 
+        // Validation
         Mockito.verify(viewContract, Mockito.times(1)).displayError();
     }
 }
