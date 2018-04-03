a6a9b1a532012dc5a8db4418fdd9ece2868e0508
Eric N <ericntd@gmail.com>
2018-04-04 00:07:34 +0800

    Added status field

--- app/src/main/java/tech/ericntd/githubsearch/models/SearchResponse.java
+++ app/src/main/java/tech/ericntd/githubsearch/models/SearchResponse.java
@@ -1,6 +1,5 @@
 package tech.ericntd.githubsearch.models;
 
-import android.content.ClipData;
 import android.support.annotation.Nullable;
 
 import com.google.gson.annotations.Expose;
@@ -9,21 +8,18 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class SearchResponse {
+    @Nullable
+    @SerializedName("total_count")
     @Expose
     private Integer totalCount;
-    @SerializedName("incomplete_results")
-    @Expose
-    private Boolean incompleteResults;
+    @Nullable
     @SerializedName("items")
     @Expose
     private List<SearchResult> searchResults = null;
 
+    @Nullable
     public Integer getTotalCount() {
         return totalCount;
-    }
-
-    public Boolean getIncompleteResults() {
-        return incompleteResults;
     }
 
     @Nullable
--- app/src/main/java/tech/ericntd/githubsearch/search/SearchViewContract.java
+++ app/src/main/java/tech/ericntd/githubsearch/search/SearchViewContract.java
@@ -1,13 +1,15 @@
 package tech.ericntd.githubsearch.search;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.util.List;
 
 import tech.ericntd.githubsearch.models.SearchResult;
 
 public interface SearchViewContract {
-    void displaySearchResults(@NonNull List<SearchResult> searchResults);
+    void displaySearchResults(@NonNull List<SearchResult> searchResults,
+                              @Nullable Integer totalCount);
 
     void displayError();
 
